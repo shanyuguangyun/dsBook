e4cece9bd563b1ac429c7d9ec35e126fda845c81
Europa <983393186@qq.com>
2023-05-03 10:19:11 +0800

    feat: 链表

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,23 +1,25 @@
 module.exports = {
   base: "/dsBook",
-  title: "数据结构",
-  description: "数据结构",
+  title: "山雨光云的技术手册",
+  description: "山雨光云的技术手册",
   
   themeConfig: {
     sidebarDepth: 3,
     nav: [
+      { text: "主页", link: "https://shanyuguangyun.github.io/webBook" },
+      { text: "基础知识", link: "https://shanyuguangyun.github.io/gitBook" },
       { text: "GitHub", link: "https://github.com/shanyuguangyun" },
     ],
     sidebar: [
       {
-        title: "数据结构", // 必要的
+        title: "dataStruct", // 必要的
         collapsable: false, // 可选的, 默认值是 true,
         sidebarDepth: 2, // 可选的, 默认值是 1
         children: [
           "/ds/xu", 
-          "/ds/linkList", 
+          "/ds/linkList"
         ],
-      },
+      }
     ],
   },
 };
